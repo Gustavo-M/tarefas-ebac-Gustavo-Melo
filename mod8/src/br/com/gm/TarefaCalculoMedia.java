0bf745e86fd543a2409c96c4f2bd9d229fe2fdec
Gustavo-M <5528005+Gustavo-M@users.noreply.github.com>
2023-07-11 22:05:18 -0300

    Subindo exercicio do Mod8

---
+++ mod8/src/br/com/gm/TarefaCalculoMedia.java
@@ -0,0 +1,17 @@
+package br.com.gm;
+
+public class TarefaCalculoMedia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nota1 = 10;
+		int nota2 = 20;
+		int nota3 = 30;
+		int nota4 = 40;
+		
+		int media = (nota1 + nota2 + nota3 + nota4)/4;
+		
+		System.out.println(media);
+	}
+
+}

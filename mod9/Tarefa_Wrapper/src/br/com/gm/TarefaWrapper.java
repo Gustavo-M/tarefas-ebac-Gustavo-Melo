001af6c69032c73b9801d06cebd6de7101600a03
Gustavo-M <5528005+Gustavo-M@users.noreply.github.com>
2023-07-11 23:28:26 -0300

    Conversão de tipos primitivos para tipo wrapper

---
+++ mod9/Tarefa_Wrapper/src/br/com/gm/TarefaWrapper.java
@@ -0,0 +1,13 @@
+package br.com.gm;
+
+public class TarefaWrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long valor1 = 903123;
+		System.out.println("primitivo " +valor1);
+		Long valor2 = valor1;
+		System.out.println("Wrapper " +valor2);
+	}
+
+}

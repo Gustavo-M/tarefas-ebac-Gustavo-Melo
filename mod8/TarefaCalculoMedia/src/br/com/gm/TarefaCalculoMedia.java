f6b58215bc9cd75d719eef4637a18a3b17d70ad1
Gustavo-M <5528005+Gustavo-M@users.noreply.github.com>
2023-07-11 23:34:08 -0300

    Atualizando valores das notas

--- mod8/TarefaCalculoMedia/src/br/com/gm/TarefaCalculoMedia.java
+++ mod8/TarefaCalculoMedia/src/br/com/gm/TarefaCalculoMedia.java
@@ -4,12 +4,12 @@ public class TarefaCalculoMedia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int nota1 = 10;
-		int nota2 = 20;
-		int nota3 = 30;
-		int nota4 = 40;
+		double nota1 = 1.0;
+		double nota2 = 2.0;
+		double nota3 = 3.0;
+		double nota4 = 4.0;
 		
-		int media = (nota1 + nota2 + nota3 + nota4)/4;
+		double media = (nota1 + nota2 + nota3 + nota4)/4;
 		
 		System.out.println(media);
 	}
